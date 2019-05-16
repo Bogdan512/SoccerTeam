27ea91f4c43b247c77e18111cc8bdfa672fd9344
Bogdan512 <ciurtinbogdan@yahoo.com>
2019-05-16 21:29:19 +0300

    Print and ReadTeams Methods for Team Class

---
+++ SoccerTeam/SoccerTeam/Team.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SoccerTeam
+{
+    public class Team
+    {
+        string name;
+        int points;
+
+        public Team()
+        {
+
+        }
+
+        public Team(string name, int points)
+        {
+            this.name = name;
+            this.points = points;
+        }
+
+        public void Print(Team[] teamsRanking)
+        {
+        for (int i = 0; i < teamsRanking.Length; i++)
+            Console.WriteLine(teamsRanking[i].name + "- " + teamsRanking[i].points);
+        }
+
+        public Team[] ReadTeams()
+        {
+            int numberOfTeams = Convert.ToInt32(Console.ReadLine());
+            Team[] result = new Team[numberOfTeams];
+            for (int i = 0; i < result.Length; i++)
+            {
+                string[] teamData = Console.ReadLine().Split('-');
+                int points = Convert.ToInt32(teamData[1]) + Convert.ToInt32(teamData[2]);
+                result[i] = new Team(teamData[0], points);
+            }
+            return result;
+        }
+    }
+}
---
+++ SoccerTeam/SoccerTeamTests/UnitTest1.cs
@@ -0,0 +1,27 @@
+using System;
+using Xunit;
+
+namespace SoccerTeam.Tests
+{
+    public class TeamTests
+    {
+        [Fact]
+        public void Test1()
+        {
+            //Team[] team = new Team[]
+            //{
+            //    new Team( "CFR", 35 ),
+            //    new Team( "UCLUJ",33 ),
+            //    new Team( "Steaua",28 ),
+            //};
+
+            //Team[] expected = new Team[]
+            //{
+            //    new Team( "CFR", 35 ),
+            //    new Team( "UCLUJ",33 ),
+            //    new Team( "Steaua",28 ),
+            //};
+            //Assert.Equal(expected, team);
+        }
+    }
+}

8e7e1d6b2f51276bebf72a1af019e575987635fb
Bogdan512 <ciurtinbogdan@yahoo.com>
2019-05-18 10:45:56 +0300

    Changed The Print Method In NonStatic

---
+++ soccerTeam/SoccerTeam/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace SoccerTeam
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Team team = new Team("A", 4);
+            Team[] aTeam = new Team[]
+            {
+                new Team( "CFR", 35 ),
+                new Team( "UCLUJ",33 ),
+                new Team( "Steaua",28 ),
+            };
+            string[] st = team.Print(aTeam);
+            foreach(string s in st)
+            {
+                Console.WriteLine(s);
+            }
+            
+            Console.Read();
+        }
+    }
+}
---
+++ soccerTeam/SoccerTeam/Team.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SoccerTeam
+{
+    public class Team
+    {
+        string name;
+        int points;
+
+        public Team(string name, int points)
+        {
+            this.name = name;
+            this.points = points;
+        }
+
+        public string[] Print(Team[] teamsRanking)
+        {
+            string[] result = new string[teamsRanking.Length];
+            for (int i = 0; i < teamsRanking.Length; i++)
+                result[i] = (teamsRanking[i].name + "- " + teamsRanking[i].points);
+            return result;
+        }
+    }
+}
